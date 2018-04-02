acada85ccd6a4960485aefd06c4eda158c4b9d78
Yuta WATANABE <yuta.watanabe@jp.fujitsu.com>
2018-04-02 16:58:26 +0900

    データベース関係の処理を実装した

---
+++ store_database.js
@@ -0,0 +1,99 @@
+/**********************************
+* 各種モジュールの読み込み
+**********************************/
+var mysql = require('mysql');
+var fs = require('fs');
+
+/**********************************
+* メイン処理
+**********************************/
+// ファイルを読み込む
+var loadFile = function () {
+	return new Promise(function (resolve, reject) {
+		// racelist.jsonをロード
+		var data = fs.readFileSync(__dirname + '/odds.json', 'utf-8');
+		// JSON形式に変換
+		var json = JSON.parse(data);
+
+		resolve(json);
+	});
+};
+
+
+// DBにデータを挿入する
+var insertToDB = function (data) {
+	return new Promise(function (resolve, reject) {
+		// DBに接続
+		var connection = mysql.createConnection({
+			host     : 'localhost',
+			user     : 'root',
+			password : 'unkoburiburi',
+			database : 'odds'
+		});
+
+		// 挿入するデータセットを作成
+		var data_set = [];
+		for (race_id in data) {
+			data[race_id].forEach(function(elem) {
+				var array = [];
+				array.push(race_id);
+				array.push(elem['wakuban']);
+				array.push(elem['umaban']);
+				array.push(elem['tan_odds']);
+				array.push(elem['fuku_odds']);
+				data_set.push(array);
+			});
+		}
+
+		console.log(data_set);
+
+		// クエリを実行
+		var query = 'INSERT INTO main (race_id, wakuban, umaban, tan_odds, fuku_odds) VALUES ?';
+		connection.query(query, [data_set], (err, rows) => {
+			if(err) throw err;
+		});
+
+		// クエリを実行
+		// connection.query('SELECT * FROM main', (err, rows) => {
+		// 	if(err) throw err;
+
+		// 	console.log(rows);
+		// });
+
+		// DBから切断
+		connection.end((err) => {
+			if (err) throw err;
+		});
+
+		resolve();
+	});
+}
+
+
+// メイン処理を実行する
+var main = function () {
+	return new Promise(function (resolve, reject) {
+
+	Promise.resolve()
+		.then(function () {
+			return loadFile();
+		})
+		.then(function (results) {
+			return insertToDB(results);
+		})
+		// .then(function (results) {
+		// 	return writeFile(results);
+		// })
+		.then(function (results) {
+			// 完了
+			resolve();
+		})
+		.catch(function (err) {
+			// エラー通知
+			reject(err);
+		});
+
+	});
+};
+
+main();
